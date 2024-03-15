945c3e0ea488387f4061c7d7865f5d36ac28defc
godud5136 <ees238@gloud.io>
2024-03-15 10:08:01 +0900

    add: vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { Reset } from 'styled-reset'
+import { Analytics } from '@vercel/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <Reset />
+    <Analytics />
     <App />
   </React.StrictMode>,
 )
